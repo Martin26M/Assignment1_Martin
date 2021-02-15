e1a9cd350758f0ee00b778638981ff6494019351
Martin <martmathew26@gmail.com>
2021-02-15 19:18:03 +0530

    First Commit

---
+++ src/main/java/com/qa/wallethub/base/Base.java
@@ -0,0 +1,60 @@
+package com.qa.wallethub.base;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	
+	public static Properties prop;
+	public static WebDriver driver;
+	public Base() throws IOException 
+	
+	{
+	FileInputStream fis = new FileInputStream("./data.properties");
+	
+	prop.load(fis);
+	
+	
+	}
+	
+	public void Initialize() {
+		
+		{
+			   String BrowserName = 	prop.getProperty("browser");
+				
+				if(BrowserName.equalsIgnoreCase("Chrome"))
+					
+				{
+					
+					System.setProperty("webdriver.chrome.driver", "C:\\Users\\HP\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+					  driver = new ChromeDriver();
+					
+					
+				}
+				
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/qa/wallethub/pages/Home.java
@@ -0,0 +1,5 @@
+package com.qa.wallethub.pages;
+
+public class Home {
+
+}
